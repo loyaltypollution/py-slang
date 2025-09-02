2f54b45de86a572e5e0871926d6f99abda27e3a0
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-09-02 15:47:47 +0000

    Use polyfill in rollup

--- rollup.config.js
+++ rollup.config.js
@@ -3,39 +3,41 @@ import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
 import wasm from '@rollup/plugin-wasm';
 import commonjs from '@rollup/plugin-commonjs';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = [
   {
-  input: 'src/conductor/PyEvaluator.ts',
-  output: {
-    file: 'dist/python-evaluator.cjs',
-    format: 'cjs',
-    name: 'PySlangRunner',
-    sourcemap: true
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/worker.js',
+      format: 'iife',
+      name: 'PySlangWorker',
+      sourcemap: true
+    },
+    plugins: [wasm(), commonjs(), json(), typescript(), nodeResolve(), nodePolyfills()]
   },
-  plugins: [wasm(), commonjs(), json(), typescript(), nodeResolve()]
-}, {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/worker.js',
-    format: 'iife',
-    name: 'PySlangWorker',
-    sourcemap: true
+  {
+    input: 'src/conductor/PyEvaluator.ts',
+    output: {
+      file: 'dist/python-evaluator.cjs',
+      format: 'cjs',
+      name: 'PySlangRunner',
+      sourcemap: true
+    },
+    plugins: [wasm(), commonjs(), json(), typescript(), nodeResolve(), nodePolyfills()]
   },
-  plugins: [wasm(), commonjs(), json(), typescript(), nodeResolve()]
-}, 
-{
-  input: 'src/sinter/sinter.ts',
-  output: {
-    file: 'dist/sinter.js',
-    format: 'cjs',
-    name: 'Sinter',
-  },
-  external: ['fs', 'path'],
-  plugins: [wasm(), commonjs(), typescript()]
-}];
+  {
+    input: 'src/sinter/sinter.ts',
+    output: {
+      file: 'dist/sinter.js',
+      format: 'cjs',
+      name: 'Sinter',
+    },
+    external: ['fs', 'path'],
+    plugins: [wasm(), commonjs(), typescript()]
+  }];
 
 export default config;
